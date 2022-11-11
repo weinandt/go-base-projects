695ca0897d65d2b98f4276eb830c959491c67400
weinandt <nick.weinandt@gmail.com>
2022-11-11 10:46:21 -0600

    added example of manual context cancellation

--- http-with-timeout/main.go
+++ http-with-timeout/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"io"
 	"log"
@@ -45,8 +46,34 @@ func makeCallOrTimeOutUsingTimePackage(timeToWaitInMs uint) (string, error) {
 	}
 }
 
+func makeCallOrTimeoutManualContextCancellation(timeToWaitInMs uint) (string, error) {
+	cxtTimeout, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(timeToWaitInMs))
+
+	// Cancel must be called to avoid leaking reasources.
+	defer cancel()
+
+	// Becuase it is possible the request times out before the channel is written to,
+	// we need to make sure writing to the channel after the context is timedout does not block the go routine.
+	// Could also use a buffered channel of size 1 here.
+	// as writing to a buffered channel will not block if the buffer is not full.
+	var res string
+	var err error
+	done := make(chan struct{})
+	go func() {
+		res, err = makeRequest()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return res, err
+	case <-cxtTimeout.Done():
+		return "", errors.New("Timed out")
+	}
+}
+
 func main() {
-	res, err := makeCallOrTimeOutUsingTimePackage(1000)
+	res, err := makeCallOrTimeoutManualContextCancellation(1000)
 	if err != nil {
 		log.Fatal("Problem making request", err)
 	}
