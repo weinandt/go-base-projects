857b61f32d45c873b144f3114744f6709f1387c7
weinandt <nick.weinandt@gmail.com>
2022-11-15 08:41:36 -0600

    started mmap work

--- mmap/main.go
+++ mmap/main.go
@@ -14,13 +14,15 @@ func main() {
 		log.Fatal("Problem opening file.", err)
 	}
 
+	// TODO: figure out if you want to use Map private or map shared.
 	// Todo: figure out this call.
-	bytes, err := syscall.Mmap(int(f.Fd()), 0, 10000, syscall.PROT_WRITE, syscall.MAP_PRIVATE)
+	bytes, err := syscall.Mmap(int(f.Fd()), 0, 10000, syscall.PROT_WRITE|syscall.PROT_READ|syscall.PROT_GROWSUP, syscall.MAP_SHARED)
 	if err != nil {
 		log.Fatal("Problem with mmap", err)
 	}
 
 	log.Println("length: ", len(bytes))
+	log.Println(bytes[0])
 
 	// Not using defer so we can log if there is an error closing the file.
 	if err = f.Close(); err != nil {
