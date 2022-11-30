320e1e9bbc5c5fc9e4f9e5c7b9915631091db969
weinandt <nick.weinandt@gmail.com>
2022-11-30 08:50:41 -0600

    added more comments

--- sha/main.go
+++ sha/main.go
@@ -27,19 +27,18 @@ func main() {
 	// The same buffer is re-used to avoid memory allocations.
 	buffer := make([]byte, 4*1024)
 	for {
+		// This could probably be done more efficiently.
+		// While writing the bytes and computing the hash, we could be going back to the disk to read more information.
 		numBytes, err := io.CopyBuffer(hasher, file, buffer)
-		if err == io.EOF {
-			break
+		if err != nil {
+			// Don't need to check for EOF.
+			// From golang docs: "A successful Copy returns err == nil, not err == EOF. Because Copy is defined to read from src until EOF, it does not treat an EOF from Read as an error to be reported."
+			log.Fatal("Problem copying buffer", err)
 		}
 
 		if numBytes == 0 {
 			break
 		}
-
-		if err != nil {
-			log.Fatal("Problem copying buffer", err)
-		}
-
 	}
 
 	sum := hasher.Sum(nil)
