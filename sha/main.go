18d1ab3020c55bd8f17e3b1abcfb647fb99796b7
weinandt <nick.weinandt@gmail.com>
2022-11-30 08:31:25 -0600

    Able to compute sha on file

--- sha/main.go
+++ sha/main.go
@@ -3,18 +3,44 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"io"
 	"log"
+	"os"
 )
 
 func main() {
-	bytes := []byte("nick♥")
-	hasher := sha1.New()
-	n, err := hasher.Write(bytes)
+	// Opening the file
+	file, err := os.Open("testFile.txt")
 	if err != nil {
-		log.Fatal("Chould not generate hash", err)
+		log.Fatal("Could not open file", err)
 	}
 
-	log.Println("number of bytes written", n)
+	// Defer closing the file, but also log any error in doing so.
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal("Problem closing the file", err)
+		}
+	}()
+
+	hasher := sha1.New()
+
+	// The same buffer is re-used to avoid memory allocations.
+	buffer := make([]byte, 4*1024)
+	for {
+		numBytes, err := io.CopyBuffer(hasher, file, buffer)
+		if err == io.EOF {
+			break
+		}
+
+		if numBytes == 0 {
+			break
+		}
+
+		if err != nil {
+			log.Fatal("Problem copying buffer", err)
+		}
+
+	}
 
 	sum := hasher.Sum(nil)
 	result := hex.EncodeToString(sum)
