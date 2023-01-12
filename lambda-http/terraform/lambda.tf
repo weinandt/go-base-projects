f671a5ef3b7e0d728ba01c6c9395ca948c1f352f
weinandt <nick.weinandt@gmail.com>
2023-01-12 08:14:35 -0600

    able to deploy lambda with terraform

--- lambda-http/terraform/lambda.tf
+++ lambda-http/terraform/lambda.tf
@@ -33,7 +33,7 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 resource "aws_lambda_function" "http_lambda" {
   filename         = "api.zip"
   function_name    = "http-lambda"
-  handler          = "api"
+  handler          = "api" # For golang, the handler function must match the name of the binary present when the archive is unziped.
   role             = aws_iam_role.lambda_role.arn
   runtime          = "go1.x"
   source_code_hash = filebase64sha256("api.zip")
